8bb1b34169694b6c46317333ef6c2fdfe7d799ce
Crimson Zero <81848256+Crimson-Zero@users.noreply.github.com>
2022-12-11 23:26:46 +0500

    Add files via upload

---
+++ Front-End/task_tracker/src/App.js
@@ -0,0 +1,19 @@
+import './styles/App.css'
+import TaskForm from './components/TaskForm';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p id ="Heading">
+          Welcome to the Task Tracker Application
+        </p>
+        <TaskForm/>
+      </header>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ Front-End/task_tracker/src/components/TaskForm.js
@@ -0,0 +1,24 @@
+import '../styles/Taskform.css'
+function TaskForm(){
+    return(
+
+<form className = "TaskForm">
+  <ul className="listitem">
+    <li>
+      <label for="task">Title of the Task</label>
+      <input type="text" id="task" name="task" />
+    </li>
+    <li>
+      <label for="Description">Task Description</label>
+      <input type="text" id="Description" name="Description" />
+    </li>
+    <li>
+    <button type="submit">Add your task</button>
+    </li>
+  </ul>
+</form>
+
+    );
+}
+
+export default TaskForm
